2c1ad382d90d7cc56ba6e777a867a49085369d3c
Chris Mendes <chris@chrismendes.uk>
2020-10-23 00:09:20 +0100

    Added number/slider component

--- src/appmaker/factories/FieldFactory/FieldFactory.js
+++ src/appmaker/factories/FieldFactory/FieldFactory.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TextField from '../../../clientapps/components/TextField';
 import DateTime from '../../../clientapps/components/DateTime';
 import Checkbox from '../../../clientapps/components/Checkbox';
+import Number from '../../../clientapps/components/Number';
 
 const FieldFactory = (props) => {
 
@@ -15,6 +16,8 @@ const FieldFactory = (props) => {
     switch(type) {
       case 'text':
         return <TextField {...props} />
+      case 'integer':
+        return <Number {...props} />
       case 'datetime':
         return <DateTime {...props} />
       case 'bool':
---
+++ src/clientapps/components/Number/Number.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import MaterialTypography from '@material-ui/core/Typography';
+import MaterialSlider from '@material-ui/core/Slider';
+import MaterialInput from '@material-ui/core/Input';
+import MaterialGrid from '@material-ui/core/Grid';
+
+const Number = (props) => {
+
+  const label = (props.label) ? props.label : '';
+
+  const useStyles = makeStyles({
+    root: {
+      width: 250,
+    },
+    input: {
+      width: 42,
+    },
+  });
+  
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+    } else if (value > 100) {
+      setValue(100);
+    }
+  };
+  
+  return (
+    <React.Fragment>
+      <MaterialTypography id="input-slider" gutterBottom>
+        {label}
+      </MaterialTypography>
+      <MaterialGrid container spacing={2} alignItems="center">
+        <MaterialGrid item xs>
+          <MaterialSlider
+            value={typeof value === 'number' ? value : 0}
+            onChange={handleSliderChange}
+          />
+        </MaterialGrid>
+        <MaterialGrid item>
+          <MaterialInput
+            className={classes.input}
+            value={value}
+            margin="dense"
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 10,
+              min: 0,
+              max: 100,
+              type: "number"
+            }}
+          />
+        </MaterialGrid>
+      </MaterialGrid>
+    </React.Fragment>
+  );
+  
+}
+
+export default Number;
