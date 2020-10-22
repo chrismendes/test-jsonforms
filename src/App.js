4ec3010f55f1fcf59e81451f2c6ed00f9600dea6
Chris Mendes <chris@chrismendes.uk>
2020-10-22 01:44:55 +0100

    Tweaked folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from './components/AppBar';
-import SideMenu from './components/SideMenu';
+import AppBar from './appmaker/components/AppBar';
+import SideMenu from './appmaker/components/SideMenu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
