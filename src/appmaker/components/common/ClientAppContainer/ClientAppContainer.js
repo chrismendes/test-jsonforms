0179d424b460013202cf6bfdd483dd5f81a762dc
Chris Mendes <chris@chrismendes.uk>
2020-10-23 02:34:41 +0100

    Tweaked client app container layout style

--- src/appmaker/components/common/ClientAppContainer/ClientAppContainer.js
+++ src/appmaker/components/common/ClientAppContainer/ClientAppContainer.js
@@ -7,9 +7,11 @@ const ClientAppContainer = (props) => {
 
   return (
     <main css={css`
+      width: 1000px;
       padding: 60px 80px;
       @media (max-width: 1000px) {
         padding: 25px;
+        width: 100%;
       }
     `}>
       {props.children}
